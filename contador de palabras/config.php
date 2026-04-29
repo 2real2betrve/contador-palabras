a712153537fbb8f29d7ff61e60f830828ae1ce18
2real2betrve <oihanapps@gmail.com>
2026-04-29 08:22:47 +0200

    Delete contador de palabras directory

--- contador de palabras/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Configuración de la Base de Datos para Hostinger
-
-define('DB_HOST', 'localhost'); // Hostinger usa 'localhost' de forma interna. (Si te falla pon 'srv1511.hstgr.io')
-define('DB_NAME', 'u433490850_chesterfield');
-define('DB_USER', 'u433490850_winstonlargo');
-define('DB_PASS', 'Pakitopako123!');
-define('DB_CHARSET', 'utf8mb4');
